3e4a8beedb6044a1b61852ed5a1fa7291366b8a4
akaydilaraa <147245101+akaydilaraa@users.noreply.github.com>
2024-11-20 01:20:23 +0300

    Add files via upload

---
+++ Odev6.12/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odev6._12
+{
+    public class Ogrenci
+    {
+        public string Ad { get; set; }
+        public int Yas { get; set; }
+
+        public Ogrenci(string ad, int yas)
+        {
+            Ad = ad;
+            Yas = yas;
+        }
+
+        public string OgrenciBilgi()
+        {
+            return $"{Ad}, {Yas} yaşında";
+        }
+    }
+
+    public class Okul
+    {
+        public string Ad { get; set; }
+        public List<Ogrenci> Ogrenciler { get; set; } = new List<Ogrenci>();
+
+        public Okul(string ad)
+        {
+            Ad = ad;
+        }
+
+        public void OgrenciEkle(Ogrenci ogrenci)
+        {
+            Ogrenciler.Add(ogrenci);
+        }
+
+        public string OkulBilgi()
+        {
+            return $"{Ad} okulunda {Ogrenciler.Count} öğrenci var.";
+        }
+    }
+
+    // Kullanım
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Okul okul = new Okul("Anadolu Lisesi");
+            okul.OgrenciEkle(new Ogrenci("Ali", 16));
+            okul.OgrenciEkle(new Ogrenci("Ayşe", 17));
+
+            Console.WriteLine(okul.OkulBilgi());
+            Console.Read();
+        }
+    }
+}
