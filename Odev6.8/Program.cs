266d257361cee806e54f4f50247a991929a52919
akaydilaraa <147245101+akaydilaraa@users.noreply.github.com>
2024-11-20 01:19:49 +0300

    Add files via upload

---
+++ Odev6.8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odev6._8
+{
+    public class Calisan
+    {
+        public string Ad { get; set; }
+        public string Pozisyon { get; set; }
+
+        public Calisan(string ad, string pozisyon)
+        {
+            Ad = ad;
+            Pozisyon = pozisyon;
+        }
+
+        public string CalisanBilgi()
+        {
+            return $"{Ad}, {Pozisyon}";
+        }
+    }
+
+    public class Sirket
+    {
+        public string Ad { get; set; }
+        public List<Calisan> Calisanlar { get; set; } = new List<Calisan>();
+
+        public Sirket(string ad)
+        {
+            Ad = ad;
+        }
+
+        public void CalisanEkle(Calisan calisan)
+        {
+            Calisanlar.Add(calisan);
+        }
+    }
+
+    // Kullanım
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sirket sirket = new Sirket("TechCorp");
+            sirket.CalisanEkle(new Calisan("Ahmet", "Yazılım Mühendisi"));
+            sirket.CalisanEkle(new Calisan("Ayşe", "Proje Yöneticisi"));
+
+            Console.WriteLine($"{sirket.Ad} şirketinde {sirket.Calisanlar.Count} çalışan var.");
+            Console.Read();
+        }
+    }
+}
---
+++ Odev6.9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odev6._9
+{
+    public class Kitap
+    {
+        public string Baslik { get; set; }
+        public string Yazar { get; set; }
+
+        public Kitap(string baslik, string yazar)
+        {
+            Baslik = baslik;
+            Yazar = yazar;
+        }
+    }
+
+    public class Kutuphane
+    {
+        public string Ad { get; set; }
+        public List<Kitap> Kitaplar { get; set; } = new List<Kitap>();
+
+        public Kutuphane(string ad)
+        {
+            Ad = ad;
+        }
+
+        public void KitapEkle(Kitap kitap)
+        {
+            Kitaplar.Add(kitap);
+        }
+    }
+
+    // Kullanım
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kutuphane kutuphane = new Kutuphane("Şehir Kütüphanesi");
+            kutuphane.KitapEkle(new Kitap("Savaş ve Barış", "Tolstoy"));
+            kutuphane.KitapEkle(new Kitap("1984", "George Orwell"));
+
+            Console.WriteLine($"{kutuphane.Ad} içerisinde {kutuphane.Kitaplar.Count} kitap var.");
+            Console.Read();
+        }
+    }
+}
